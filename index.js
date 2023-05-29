662326373c60e6f72b77d52b1ef88b03b16184da
Luís Costa <67716453+Luijinho@users.noreply.github.com>
2023-05-29 22:24:10 +0100

    Changed headers

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
