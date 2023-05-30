a82fe35a1ff9161969ebda816ff598abdf3ee448
Luís Costa <67716453+Luijinho@users.noreply.github.com>
2023-05-30 17:43:23 +0100

    endpoints changed to post

--- index.js
+++ index.js
@@ -103,13 +103,13 @@ app.get('/getAllExercises', async (req, res) => {
   res.json(exercisesWithDescriptions);
 });
 
-app.get('/evaluateExercise', async (req, res) => {
+app.post('/evaluateExercise', async (req, res) => {
     const attemptData = req.body;
     const feedback = await webpal.evaluateAttempt(attemptData.id, attemptData.attemptFiles, attemptData.previousFeedback);
     res.json(feedback);
 });
 
-app.get('/evaluateExerciseWithoutStatic', async (req, res) => {
+app.post('/evaluateExerciseWithoutStatic', async (req, res) => {
     const attemptData = req.body;
     const feedback = await webpal.evaluateAttemptWithoutStatic(attemptData.id, attemptData.attemptFiles, attemptData.previousFeedback);
     res.json(feedback);
