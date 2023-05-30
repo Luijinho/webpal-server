3ffee8aacc0c5bfdb6e6aca1ade8110dd3bfb218
Luís Costa <67716453+Luijinho@users.noreply.github.com>
2023-05-30 16:51:00 +0100

    changed origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const archiver = require('archiver');
 const app = express();
 
 const corsOptions ={
-  origin:'https://luijinho.github.io', 
+  origin:'*', 
   credentials:true,
   optionSuccessStatus:200,
 }
@@ -16,7 +16,7 @@ const corsOptions ={
 app.use(cors(corsOptions));
 app.options('*', cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://luijinho.github.io');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
