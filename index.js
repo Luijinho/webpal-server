807edbbd139b750ad3a551d87b5b72f6b686ce01
Luís Costa <67716453+Luijinho@users.noreply.github.com>
2023-05-30 17:35:11 +0100

    changed cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://luijinho.github.io');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
